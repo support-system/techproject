5998bbe6fb814b212b9908ce4beb7e21d76f7940
Rahul Sharma <Rahulsh@Etechaces.com>
2018-10-29 21:38:21 +0530

    asdasdas

--- Main/wp-config.php
+++ Main/wp-config.php
@@ -20,13 +20,24 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'rahultest');
+// define('DB_NAME', 'rahultest');
+
+// /** MySQL database username */
+// define('DB_USER', 'root');
+
+// /** MySQL database password */
+// define('DB_PASSWORD', '');
+
+// /** MySQL hostname */
+// define('DB_HOST', 'localhost');
+
+define('DB_NAME', 'wp_myblog');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'mohit');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'Password@123');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
